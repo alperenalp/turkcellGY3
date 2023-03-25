9153031738b6c745c6332c485b8930ccf0659293
alperenalp <alperen.alp045@gmail.com>
2023-03-25 23:53:31 +0300

    tried to learn the concept of variant

---
+++ C#/Tasks/Task4VariantConcept/Task4VariantConcept/Animal.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task4VariantConcept
+{
+    //kovaryant
+    public class Animal
+    {
+        public virtual void Feed()
+        {
+            Console.WriteLine("Animal is being fed.");
+        }
+    }
+
+    public class Dog : Animal
+    {
+        public override void Feed()
+        {
+            Console.WriteLine("Dog is being fed.");
+        }
+    }
+
+    public class Cat : Animal
+    {
+        public override void Feed()
+        {
+            Console.WriteLine("Cat is being fed.");
+        }
+    }
+
+
+
+    //kontravaryant
+    public class HayvanBarinagi
+    {
+        private List<Animal> hayvanlar = new List<Animal>();
+
+        public virtual void EvcilHayvanEkle(Animal hayvan)
+        {
+            hayvanlar.Add(hayvan);
+        }
+    }
+
+    public class KediBarinagi : HayvanBarinagi
+    {
+        public override void EvcilHayvanEkle(Animal hayvan)
+        {
+            if (hayvan is Cat)
+            {
+                base.EvcilHayvanEkle(hayvan);
+            }
+            else
+            {
+                throw new ArgumentException("Kedi barınağına sadece Kedi nesneleri ekleyebilirsiniz.");
+            }
+        }
+    }
+
+
+    interface IAnimal<in T>
+    {
+        void MakeSound(T animal);
+    }
+
+    class AnimalSound<T> : IAnimal<T>
+    {
+        public void MakeSound(T animal)
+        {
+            Console.WriteLine("The animal made a sound.");
+        }
+    }
+
+}
---
+++ C#/Tasks/Task4VariantConcept/Task4VariantConcept/Program.cs
@@ -0,0 +1,67 @@
+﻿using Task4VariantConcept;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        /*
+         * Kovaryant, veri türlerinin birbirine uygunluğu hakkında bir kavramdır. Bu, bir veri türünün, daha geniş bir veri türünün yerine kullanılabileceği anlamına gelir. 
+         * 
+         * Kovaryantlık, alt sınıfların, üst sınıfların yerine kullanılabileceği anlamına gelir. 
+         * Yani, alt sınıflar, üst sınıfların yerine geçebilir ve üst sınıfın davranışını aynen taklit edebilirler. 
+         * Bu, bir programın daha esnek olmasını sağlar ve kodun yeniden kullanılabilirliğini artırır.
+         * 
+         * */
+
+        Animal animal = new Animal();
+        Animal dog = new Dog();
+        Animal cat = new Cat();
+
+        animal.Feed();
+        dog.Feed();
+        cat.Feed();
+
+        /*
+         * 
+         * "Animal" sınıfının bir özelliği, "Feed" metodudur. "Cat" ve "Dog" sınıfları da bu metodu kalıtım yoluyla almış olabilirler. 
+         * Bu durumda, bir "Cat" veya "Dog" nesnesi, "Animal" tipi olarak kullanılabilir ve "Feed" metodunu kullanabilir.
+         * 
+         * */
+
+
+        /*
+         * Kontravaryantlık ise, tam tersi bir durumdur. Bir fonksiyonun parametrelerindeki tiplerin, alt tiplerin yerine geçebileceği anlamına gelir. 
+         * 
+         * */
+
+        HayvanBarinagi hayvanBarinagi = new HayvanBarinagi();
+        KediBarinagi kediBarinagi = new KediBarinagi();
+
+        hayvanBarinagi.EvcilHayvanEkle(new Animal());
+        //kediBarinagi.EvcilHayvanEkle(new Animal());
+        kediBarinagi.EvcilHayvanEkle(new Cat());
+
+        /*
+         * "HayvanBarinagi" sınıfı, "EvcilHayvanEkle" metodunu tanımlar ve "hayvanlar" listesine yeni hayvan nesneleri ekler. 
+         * "KediBarinagi" sınıfı ise "EvcilHayvanEkle" metodunu ezer ve sadece "Kedi" tipindeki nesnelerin eklenmesine izin verir.
+         * Bu durumda, "EvcilHayvanEkle" metodunun parametrelerinin alt tiplerinin kullanılabilmesi nedeniyle 
+         * "HayvanBarınağı" sınıfı, "EvcilHayvanEkle" metodunun kontravaryant olmasını sağlar.
+         * */
+
+
+
+        /*
+         * In-Out generic interface'ler, generic tür parametrelerinin kovaryant ve kontravaryant olabilmesine olanak tanıdığı için, türler arasında dönüşüm yapmaya olanak sağlar.
+         * Bu durum, kodun daha esnek olmasına ve daha az tekrar edilmesine yardımcı olur.
+         * 
+         * Bir interface'deki "in" parametresinin kontravaryant olması, alt sınıf beklenen yere üst sınıf geçilebilir demektir.
+         * */
+
+        IAnimal<Animal> animalSound = new AnimalSound<Animal>();
+        IAnimal<Dog> dogSound = animalSound; // Kontravaryant kullanım
+
+        dogSound.MakeSound(new Dog()); // "The animal made a sound." çıktısını verir
+
+    }
+
+}
